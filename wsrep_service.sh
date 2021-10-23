525be7814ad9635316903adefb0d9626efbf85c0
simonyipeter <simonyip001@gmail.com>
2021-10-23 10:44:39 +0200

    Update wsrep_service.sh

--- wsrep_service.sh
+++ wsrep_service.sh
@@ -5,7 +5,7 @@ GALERA_PWD=vAqC7wdtjRcsdDG
 
 case $1 in
         bootstrap)
-mkdir -p $GALERA_DATA_ROOT_FOLDER/data && chmod 777 -R $GALERA_DATA_ROOT_FOLDER/data
+mkdir -p $GALERA_DATA_ROOT_FOLDER/data && mkdir -p $GALERA_DATA_ROOT_FOLDER/conf && chmod 777 -R $GALERA_DATA_ROOT_FOLDER/data && touch $GALERA_DATA_ROOT_FOLDER/conf/my.cnf
 docker network create -d overlay --attachable pxc-network
 docker run -d --name $GALERA_NODE_NAME-0 --net=pxc-network \
   -e MARIADB_GALERA_CLUSTER_BOOTSTRAP=yes \
@@ -26,7 +26,7 @@ docker run -d --name $GALERA_NODE_NAME-0 --net=pxc-network \
 ;;
 
 start)
-mkdir -p $GALERA_DATA_ROOT_FOLDER/data && chmod 777 -R $GALERA_DATA_ROOT_FOLDER/data
+mkdir -p $GALERA_DATA_ROOT_FOLDER/data && mkdir -p $GALERA_DATA_ROOT_FOLDER/conf && chmod 777 -R $GALERA_DATA_ROOT_FOLDER/data && touch $GALERA_DATA_ROOT_FOLDER/conf/my.cnf
 docker run -d --name $GALERA_NODE_NAME-$2 --net=pxc-network \
   -e MARIADB_GALERA_CLUSTER_NAME=galera_cluster \
   -e MARIADB_GALERA_CLUSTER_ADDRESS="gcomm://$GALERA_NODE_NAME-$3" \
